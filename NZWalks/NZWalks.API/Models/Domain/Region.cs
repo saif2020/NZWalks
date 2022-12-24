cc3e47e539bf84c52621ebf8a5802a5d6a92631c
saifeddine <saifeddineazzabi1997@gmail.com>
2022-12-24 12:38:11 +0100

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public double Area { get; set; }
+        public long Population { get; set; }
+
+        // Navigation property
+        // one region can have mutiple walks to tell entity framework
+
+        public IEnumerable<Walk> Walks { get; set; }
+
+
+    }
+}
