a9ba5d505e810ac1a10e0796006f829914eeb23c
saifeddine <saifeddineazzabi1997@gmail.com>
2023-01-08 12:59:54 +0100

    End of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("Regions")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync();
+
+            // return DTO regions
+
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id = region.Id,
+            //        Name = region.Name,
+            //        Code = region.Code,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population
+            //    };
+            //    regionsDTO.Add(regionDTO); 
+
+            //});
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Profiles/RegionsProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Models.Profiles
+{
+    public class RegionsProfile: Profile
+    {
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>();
+        }
+    }
+}
