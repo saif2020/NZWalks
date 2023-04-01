caec28ca1aa50fea9648eae96b8435dfc0dbb848
saifeddine <saifeddineazzabi1997@gmail.com>
2023-04-01 23:29:05 +0100

    walk apis

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walksDomain = await walkRepository.GetAllAsync();
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walksDomain);
+            
+            return Ok(walksDTO);
+        }
+    }
+}
